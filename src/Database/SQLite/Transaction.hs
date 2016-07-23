038b4c009b12112674b9ef729dd93d175149f9e6
Fredrik Malmros <fredrik.malmros@klarna.com>
2016-07-23 23:40:41 +0200

    expose the transaction type

--- src/Database/SQLite/Transaction.hs
+++ src/Database/SQLite/Transaction.hs
@@ -1,35 +1,56 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE General0izedNewtypeDeriving #-}
+
+-- |
+-- Module:      Database.SQLite.Transaction
+-- Copyright:   (c) 2016 Fredrik Malmros.
+-- License:     MIT
+-- Stability:   experimental
+-- Portability: GHC
 
 module Database.SQLite.Transaction ( runTransaction
+                                   , runTransaction'
                                    , Transaction(..)
+                                   , TransactionType(..)
                                    ) where
 
 import Database.SQLite
 import Control.Monad.Reader
 import Control.Exception
 
-newtype Transaction a = Transaction (ReaderT SQLiteHandle IO a )
-             deriving
-               ( Functor
-               , Applicative
-               , Monad
-               , MonadReader SQLiteHandle
-               , MonadIO)
+-- | The SQLite transaction monad transformer.
+newtype Transaction a = MkTransaction (ReaderT SQLiteHandle IO a )
+  deriving ( Functor
+           , Applicative
+           , Monad
+           , MonadReader SQLiteHandle
+           , MonadIO)
 
+-- | The SQLite transaction type. See <https://www.sqlite.org/lang_transaction.html>.
+data TransactionType = Deferred | Immediate | Exclusive
 
+instance Show TransactionType where
+  show Deferred  = "DEFERRED"
+  show Immediate = "IMMEDIATE"
+  show Exclusive = "EXCLUSIVE"
+
+-- | Run a deferred transaction.
 runTransaction ::  Transaction a -> SQLiteHandle -> IO a
-runTransaction (Transaction t) con =
+runTransaction trans con = runTransaction' Deferred trans con
+
+-- | Run a transaction of the given transaction type.
+runTransaction' :: TransactionType -> Transaction a -> SQLiteHandle -> IO a
+runTransaction' typ (MkTransaction tr) con =
   mask $ \restore -> do
-  begin con
-  res <- try $ restore (runReaderT t con)
+  begin con typ
+  res <- try $ restore (runReaderT tr con)
   case res of
     Left ex -> rollback con >> throwIO (ex ::SomeException)
     Right a -> do
       commit con
       return a
 
-begin :: SQLiteHandle -> IO ()
-begin con = void $ execStatement_ con "BEGIN TRANSACTION"
+begin :: SQLiteHandle -> TransactionType -> IO ()
+begin con ty = void $ execStatement_ con ("BEGIN TRANSACTION " ++ show ty)
 
 commit :: SQLiteHandle -> IO ()
 commit con =  void $ execStatement_ con "END TRANSACTION"
